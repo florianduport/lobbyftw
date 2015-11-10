c8a04a2d94ea8934826649b02faf7b871ac10e35
wiidii-developer <florian.duport@akka.eu>
2015-11-10 09:16:50 +0100

    correction serveur socket.io

--- public/angular/app.js
+++ public/angular/app.js
@@ -3,9 +3,13 @@
     'ngRoute',
     'ngSocket',
     'lobbyftwControllers'
-  ]).config(["$socketProvider", function ($socketProvider) {
-      $socketProvider.setUrl("http://localhost:3000");
-    }]);
+  ]).config(["$socketProvider", function($socketProvider) {
+    var socketServer = location.hostname;
+    if (socketServer.indexOf("localhost") > -1) {
+      socketServer += ":3000";
+    }
+    $socketProvider.setUrl(socketServer);
+  }]);
 
   lobbyftwApp.config(['$routeProvider',
     function($routeProvider) {
@@ -28,7 +32,7 @@
   var lobbyftwControllers = angular.module('lobbyftwControllers', []);
 
   //main page controller => global
-  lobbyftwControllers.controller('mainController', function($scope){
+  lobbyftwControllers.controller('mainController', function($scope) {
 
   });
 
@@ -44,16 +48,21 @@
     function($scope, $rootScope, $routeParams, $socket, $location, $http) {
 
       $scope.channel = 0;
-      if($routeParams !== undefined && $routeParams.id !== undefined){
-          $scope.channel = $routeParams.id;
+      if ($routeParams !== undefined && $routeParams.id !== undefined) {
+        $scope.channel = $routeParams.id;
       }
 
-      $scope.sendMessage = function(channelId, message){
-        $socket.emit('sendMessage', {channelId : channelId, user : $scope.user, messages : [message], date : new Date()});
+      $scope.sendMessage = function(channelId, message) {
+        $socket.emit('sendMessage', {
+          channelId: channelId,
+          user: $scope.user,
+          messages: [message],
+          date: new Date()
+        });
       }
-      $scope.isCurrentChannel = function(channel){
+      $scope.isCurrentChannel = function(channel) {
 
-        if($scope.channel == channel.id)
+        if ($scope.channel == channel.id)
           return "active";
         else
           return false;
@@ -63,7 +72,7 @@
       $http.get('/loadUser').success(function(data) {
         $rootScope.user = data;
 
-        if(!$rootScope.user){
+        if (!$rootScope.user) {
           $location.path('/');
         }
 
@@ -76,12 +85,12 @@
           $scope.chat = data;
           $scope.currentChannel;
           for (var channelEl in $scope.chat.channels) {
-            if($scope.chat.channels[channelEl].id == $scope.channel){
+            if ($scope.chat.channels[channelEl].id == $scope.channel) {
               $scope.currentChannel = $scope.chat.channels[channelEl];
             }
           }
           var height = jQuery("#chatzone_content")[0].scrollHeight;
-          jQuery("#chatzone_content").scrollTop(height+150);
+          jQuery("#chatzone_content").scrollTop(height + 150);
         });
       });
 
