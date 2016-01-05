0b4665b44423c148cbbb8fcfc1f32abeecc2c659
adriend <adrien.dhuicq@viacesi.fr>
2016-01-05 23:38:20 +0100

    fix send message

--- public/javascripts/chatclient.js
+++ public/javascripts/chatclient.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
    // where idOfYourVideo is the YouTube ID.
-  $("body").delegate('#textbox textarea', 'keydown', function(event) {
+  $("body").delegate('.chatzone_textarea', 'keydown', function(event) {
       // Check the keyCode and if the user pressed Enter (code = 13)
       if (event.keyCode == 13) {
           event.preventDefault();
-          angular.element('#container').scope().sendMessage($('#channelName').val(), $('#textbox textarea').val());
-          $('#textbox textarea').val("")
+          angular.element('#container').scope().sendMessage($('#channelName').val(), $('.chatzone_textarea').val());
+          $('.chatzone_textarea').val("");
           var height = jQuery("#chatzone_content")[0].scrollHeight;
           jQuery("#chatzone_content").scrollTop(height+150);
       }
